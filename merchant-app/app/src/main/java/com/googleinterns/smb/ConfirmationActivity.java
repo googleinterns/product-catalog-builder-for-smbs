765afb9aa35213d8b86e11e9a11041515106e628
Raghunathan <braghunathan@noogler.corp-partner.google.com>
2020-05-21 09:47:49 +0530

    Add navigation from text scan to confirmation activity

--- merchant-app/app/src/main/java/com/googleinterns/smb/ConfirmationActivity.java
+++ merchant-app/app/src/main/java/com/googleinterns/smb/ConfirmationActivity.java
@@ -4,6 +4,7 @@ package com.googleinterns.smb;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -12,6 +13,8 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.googleinterns.smb.adapter.EANAdapter;
+import com.googleinterns.smb.adapter.ProductConfirmationAdapter;
+import com.googleinterns.smb.model.Product;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -21,8 +24,10 @@ public class ConfirmationActivity extends AppCompatActivity {
 
     private static final String TAG = ConfirmationActivity.class.getName();
     public static final String DETECTED_BARCODES = "DETECTED_BARCODES";
+    public static final String DETECTED_PRODUCTS = "DETECTED_PRODUCTS";
 
-    private EANAdapter mAdapter;
+    private EANAdapter mEANAdapter;
+    private ProductConfirmationAdapter mProductAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,23 +38,33 @@ public class ConfirmationActivity extends AppCompatActivity {
     }
 
     /**
-     * Fetch data from firebase and initialise recycler view
+     * Fetch data and initialise recycler view
      */
     private void initRecyclerView() {
-        // get barcodes from launcher activity
-        List<String> barcodes = getBarcodes();
-        if (barcodes.size() == 0) {
+
+        RecyclerView.Adapter adapter;
+        if (getIntent().hasExtra(DETECTED_BARCODES)) {
+            List<String> barcodes;
+            try {
+                barcodes = getBarcodes();
+            } catch (AssertionError ae) {
+                barcodes = new ArrayList<>();
+            }
+            adapter = getAdapterFromBarcodes(barcodes);
+        } else if (getIntent().hasExtra(DETECTED_PRODUCTS)) {
+            // load ProductConfirmationAdapter (render products directly)
+            List<Product> products = getProducts();
+            adapter = getAdapterFromProducts(products);
+            Log.d(TAG, "Product list");
+            for (int i = 0; i < products.size(); i++) {
+                Log.d(TAG, "Product: " + products.get(i).getProductName());
+            }
+        } else {
             return;
         }
-        Query query = FirebaseFirestore.getInstance().collection("products");
-        // filter products containing these barcodes
-        query = query.whereIn("EAN", barcodes);
-
-
         // initialize recycler view
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        mAdapter = new EANAdapter(query, getSupportFragmentManager());
-        recyclerView.setAdapter(mAdapter);
+        recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this) {
             @Override
             public boolean supportsPredictiveItemAnimations() {
@@ -58,19 +73,46 @@ public class ConfirmationActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Create recycler view adapter directly using barcodes. EANAdapter inherits from FirestoreAdapter and displays realtime data.
+     *
+     * @param barcodes list of retrieved barcodes
+     * @return Recycler view adapter
+     * @throws AssertionError
+     */
+    private RecyclerView.Adapter getAdapterFromBarcodes(List<String> barcodes) throws AssertionError {
+        if ((barcodes.size() == 0)) throw new AssertionError("Barcode list must be non-empty");
+        Query query = FirebaseFirestore.getInstance().collection("products");
+        // filter products containing these barcodes
+        query = query.whereIn("EAN", barcodes);
+        mEANAdapter = new EANAdapter(query, getSupportFragmentManager());
+        return mEANAdapter;
+    }
+
+    /**
+     * Create recycler view adapter from products
+     *
+     * @param products list of retrieved products
+     * @return recycler view adapter
+     */
+    private RecyclerView.Adapter getAdapterFromProducts(List<Product> products) {
+        mProductAdapter = new ProductConfirmationAdapter(products, getSupportFragmentManager());
+        return mProductAdapter;
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
-        if (mAdapter != null) {
-            mAdapter.startListening();
+        if (mEANAdapter != null) {
+            mEANAdapter.startListening();
         }
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (mAdapter != null) {
-            mAdapter.stopListening();
+        if (mEANAdapter != null) {
+            mEANAdapter.stopListening();
         }
     }
 
@@ -86,19 +128,43 @@ public class ConfirmationActivity extends AppCompatActivity {
             }
         } catch (Exception e) {
             barcodes = new ArrayList<>();
+            Log.e(TAG, "Error loading barcodes from intent");
         }
         return barcodes;
     }
 
     /**
+     * utility to get products from serialised data in intent.
+     */
+    private List<Product> getProducts() {
+        List<Product> products;
+        try {
+            products = (List<Product>) getIntent().getSerializableExtra(DETECTED_PRODUCTS);
+            if (products == null) {
+                products = new ArrayList<>();
+            }
+        } catch (Exception e) {
+            products = new ArrayList<>();
+            Log.e(TAG, "Error loading products from intent");
+        }
+        return products;
+    }
+
+    /**
      * Make intent for confirmation activity.
+     * To display scanned products and confirmation to add
      *
      * @param context  initiater activity
      * @param barcodes list of detected barcodes to display
      * @return intent to start Confirmation activity
      */
-    public static Intent makeIntent(Context context, List<String> barcodes) {
+    public static Intent makeIntentFromBarcodes(Context context, List<String> barcodes) {
         return new Intent(context, ConfirmationActivity.class)
                 .putExtra(DETECTED_BARCODES, (Serializable) barcodes);
+    }
+
+    public static Intent makeIntentFromProducts(Context context, List<Product> products) {
+        return new Intent(context, ConfirmationActivity.class)
+                .putExtra(DETECTED_PRODUCTS, (Serializable) products);
     }
 }
