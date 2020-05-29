a0d0cd0c4a20ac20fb02c585fcb4fb6a9d3fa045
Raghu Nathan <raghu15sep99@gmail.com>
2020-05-29 06:52:28 +0530

    Add barcode detection support using the MLKit APIs

---
+++ merchant-app/app/src/main/java/com/googleinterns/smb/ScanActivity.java
@@ -0,0 +1,227 @@
+package com.googleinterns.smb;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.googleinterns.smb.barcodescanning.BarcodeScanningProcessor;
+import com.googleinterns.smb.common.CameraSource;
+import com.googleinterns.smb.common.CameraSourcePreview;
+import com.googleinterns.smb.common.GraphicOverlay;
+import com.googleinterns.smb.common.preference.SettingsActivity;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ScanActivity extends AppCompatActivity
+        implements ActivityCompat.OnRequestPermissionsResultCallback {
+
+    private static final String TAG = ScanActivity.class.getName();
+    private static final int PERMISSION_REQUESTS = 1;
+    public static final String DETECTED_BARCODES = "DETECTED_BARCODES";
+
+    private CameraSource cameraSource = null;
+    private GraphicOverlay fireFaceOverlay;
+    private CameraSourcePreview firePreview;
+    private BarcodeScanningProcessor mDetector = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.activity_scan);
+        fireFaceOverlay = (GraphicOverlay) findViewById(R.id.fireFaceOverlay);
+        firePreview = (CameraSourcePreview) findViewById(R.id.firePreview);
+        if (allPermissionsGranted()) {
+            createCameraSource();
+        } else {
+            getRuntimePermissions();
+        }
+    }
+
+    private void createCameraSource() {
+        // If there's no existing cameraSource, create one.
+        if (cameraSource == null) {
+            cameraSource = new CameraSource(this, fireFaceOverlay);
+        }
+        Log.i(TAG, "Using Image Label Detector Processor");
+        try {
+            mDetector = new BarcodeScanningProcessor();
+            cameraSource.setMachineLearningFrameProcessor(mDetector);
+        } catch (Exception e) {
+            Log.e(TAG, "Can not create image processor", e);
+            Toast.makeText(
+                    getApplicationContext(),
+                    "Can not create image processor" + e.getMessage(),
+                    Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    /**
+     * Add settings option in the menu bar
+     *
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.live_preview_menu, menu);
+        return true;
+    }
+
+
+    /**
+     * Called when settings option is selected from menu
+     *
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.settings) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            intent.putExtra(SettingsActivity.EXTRA_LAUNCH_SOURCE, SettingsActivity.LaunchSource.LIVE_PREVIEW);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Starts or restarts the camera source, if it exists. If the camera source doesn't exist yet
+     * (e.g., because onResume was called before the camera source was created), this will be called
+     * again when the camera source is created.
+     */
+    private void startCameraSource() {
+        if (cameraSource != null) {
+            try {
+                if (firePreview == null) {
+                    Log.d(TAG, "resume: Preview is null");
+                }
+                if (fireFaceOverlay == null) {
+                    Log.d(TAG, "resume: graphOverlay is null");
+                }
+                firePreview.start(cameraSource, fireFaceOverlay);
+            } catch (IOException e) {
+                Log.e(TAG, "Unable to start camera source.", e);
+                cameraSource.release();
+                cameraSource = null;
+            }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+        startCameraSource();
+    }
+
+    /**
+     * Stops the camera.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        firePreview.stop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (cameraSource != null) {
+            cameraSource.release();
+        }
+    }
+
+    /**
+     * Finish scanning process
+     *
+     * @return
+     */
+    public void finishScan(View view) {
+        firePreview.stop();
+        if (cameraSource != null) {
+            cameraSource.release();
+        }
+        // pass the detected barcodes to confirmation activity (to remove if any unwanted detected)
+        Intent intent = new Intent(this, ConfirmationActivity.class)
+                .putExtra(DETECTED_BARCODES, (Serializable) mDetector.getDetectedBarCodes());
+        startActivity(intent);
+    }
+
+    private String[] getRequiredPermissions() {
+        try {
+            PackageInfo info =
+                    this.getPackageManager()
+                            .getPackageInfo(this.getPackageName(), PackageManager.GET_PERMISSIONS);
+            String[] ps = info.requestedPermissions;
+            if (ps != null && ps.length > 0) {
+                return ps;
+            } else {
+                return new String[0];
+            }
+        } catch (Exception e) {
+            return new String[0];
+        }
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void getRuntimePermissions() {
+        List<String> allNeededPermissions = new ArrayList<>();
+        for (String permission : getRequiredPermissions()) {
+            if (!isPermissionGranted(this, permission)) {
+                allNeededPermissions.add(permission);
+            }
+        }
+
+        if (!allNeededPermissions.isEmpty()) {
+            ActivityCompat.requestPermissions(
+                    this, allNeededPermissions.toArray(new String[0]), PERMISSION_REQUESTS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, String[] permissions, @NonNull int[] grantResults) {
+        Log.i(TAG, "Permission granted!");
+        if (allPermissionsGranted()) {
+            createCameraSource();
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private static boolean isPermissionGranted(Context context, String permission) {
+        if (ContextCompat.checkSelfPermission(context, permission)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission granted: " + permission);
+            return true;
+        }
+        Log.i(TAG, "Permission NOT granted: " + permission);
+        return false;
+    }
+}
