d1d1615c1e22c4d255842a27c6a177d74f2de5b3
Raghu Nathan <braghunathan@noogler.corp-partner.google.com>
2020-05-29 17:58:43 +0530

    Enable the user to set the discounted price and remove the product from inventory (#12)

--- merchant-app/app/src/main/java/com/googleinterns/smb/barcodescanning/BarcodeScanningProcessor.java
+++ merchant-app/app/src/main/java/com/googleinterns/smb/barcodescanning/BarcodeScanningProcessor.java
@@ -32,6 +32,7 @@ import com.googleinterns.smb.common.CameraImageGraphic;
 import com.googleinterns.smb.common.FrameMetadata;
 import com.googleinterns.smb.common.GraphicOverlay;
 
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
---
+++ merchant-app/app/src/main/java/com/googleinterns/smb/model/Product.java
@@ -0,0 +1,74 @@
+package com.googleinterns.smb.model;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+
+public class Product {
+
+    private String productName;
+    private Double MRP;
+    private Double discountedPrice;
+    private String imageURL;
+    private static final String RUPEE = "\u20b9";
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getMRP() {
+        return MRP;
+    }
+
+    public String getMRPString() {
+        return RUPEE + " " + MRP.toString();
+    }
+
+    public void setMRP(Double MRP) {
+        this.MRP = MRP;
+    }
+
+    public Double getDiscountedPrice() {
+        return discountedPrice;
+    }
+
+    public String getDiscountedPriceString() {
+        return RUPEE + " " + discountedPrice.toString();
+    }
+
+    public void setDiscountedPrice(Double discountedPrice) {
+        this.discountedPrice = discountedPrice;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+
+    /**
+     * Empty constructor required by Firebase
+     */
+    public Product() {
+
+    }
+
+    /**
+     * Initialise product using firebase documentSnapshot
+     *
+     * @param documentSnapshot document snapshot from firebase
+     */
+    public Product(DocumentSnapshot documentSnapshot) {
+        productName = documentSnapshot.getString("product_name");
+        MRP = documentSnapshot.getDouble("MRP");
+        // Initialise discounted price to be same as MRP
+        discountedPrice = MRP;
+        imageURL = documentSnapshot.getString("image_url");
+    }
+
+}
