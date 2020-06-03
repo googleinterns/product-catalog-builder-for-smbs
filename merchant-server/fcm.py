88900d4468e07e2a9731f096b421b1266f9bd27b
Raghu Nathan <braghunathan@noogler.corp-partner.google.com>
2020-06-03 11:29:04 +0530

    Add Customer Order Confirmation screen via the Notification flow

---
+++ merchant-server/fcm.py
@@ -0,0 +1,19 @@
+from pyfcm import FCMNotification
+from keys import FCM_API_KEY
+from constants import PLACE_ORDER
+
+push_service = FCMNotification(api_key=FCM_API_KEY)
+
+def notify(token):
+    message_title = "Test notification"
+    message_body = "Hi, this is a test notification"
+    result = push_service.notify_single_device(registration_id=token, message_title=message_title, message_body=message_body)
+    return result
+
+def notify_place_order(token, data):
+    data['type'] = PLACE_ORDER
+    result = push_service.notify_single_device(
+        registration_id=token,
+        data_message=data
+        )
+    return result
