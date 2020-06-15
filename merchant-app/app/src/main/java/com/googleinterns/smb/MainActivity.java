eb88285f35339fa67e2370163ed972fabcb9f34b
Raghunathan <braghunathan@noogler.corp-partner.google.com>
2020-06-15 12:18:17 +0530

    Revert "Add support for Google Sign In support using Firebase Auth(#10)"
    This reverts commit 87560af8b55c729c68b1cebc48dbddcfbaeb060d.

--- merchant-app/app/src/main/java/com/googleinterns/smb/MainActivity.java
+++ merchant-app/app/src/main/java/com/googleinterns/smb/MainActivity.java
@@ -1,5 +1,6 @@
 package com.googleinterns.smb;
 
+
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -8,25 +9,18 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-import com.firebase.ui.auth.AuthUI;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.googleinterns.smb.barcodescanning.BarcodeScanningProcessor;
 import com.googleinterns.smb.barcodescanning.BarcodeStatusListener;
 import com.googleinterns.smb.common.VideoToBarcode;
 import com.googleinterns.smb.fragment.AddProductDialogFragment;
 
 import java.io.IOException;
-import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements
@@ -34,62 +28,17 @@ public class MainActivity extends AppCompatActivity implements
 
     private static final String TAG = MainActivity.class.getName();
     private static final int PICK_IMAGE = 1;
-    private static final int PICK_VIDEO = 2;
-    private static final int START_SIGN_IN = 3;
 
+    private static final int PICK_VIDEO = 2;
     // video to barcode converter task
     private AsyncTask<?, ?, ?> task;
     private DialogFragment mDialogFragment;
-    private boolean isSigningIn = false;
 
     @Override
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Enable Firestore logging
-        FirebaseFirestore.setLoggingEnabled(true);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.menu_sign_out) {
-            AuthUI.getInstance().signOut(this);
-            startSignIn();
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        // Start sign in if necessary
-        if (isSignInRequired()) {
-            startSignIn();
-        }
-    }
-
-    private boolean isSignInRequired() {
-        return (!isSigningIn && FirebaseAuth.getInstance().getCurrentUser() == null);
-    }
-
-    /**
-     * Initiates google auth sign in with firebase UI
-     */
-    private void startSignIn() {
-        Intent intent = AuthUI.getInstance().createSignInIntentBuilder()
-                .setAvailableProviders(Collections.singletonList(
-                        new AuthUI.IdpConfig.GoogleBuilder().build()))
-                .setIsSmartLockEnabled(false)
-                .build();
-        isSigningIn = true;
-        startActivityForResult(intent, START_SIGN_IN);
     }
 
     /**
@@ -159,13 +108,6 @@ public class MainActivity extends AppCompatActivity implements
             } catch (Exception e) {
                 showToast(getString(R.string.error_processing_video));
                 Log.d(TAG, "Error while loading file: ", e);
-            }
-        } else if (requestCode == START_SIGN_IN) {
-            isSigningIn = false;
-            if (resultCode != RESULT_OK && isSignInRequired()) {
-                startSignIn();
-            } else if (resultCode == RESULT_OK) {
-                showToast("Sign In successful!");
             }
         }
     }
