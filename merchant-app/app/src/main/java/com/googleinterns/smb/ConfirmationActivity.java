c87f2558fedf4b383db3056b98cf1088d5fe7647
Raghunathan <braghunathan@noogler.corp-partner.google.com>
2020-06-15 12:18:17 +0530

    Revert "Integrated Product Display in Card View using the Firebase Cloud Firestore (#11)"
    This reverts commit 7e7c792c767ad273a78869dbc73e967fb34ad659.

--- merchant-app/app/src/main/java/com/googleinterns/smb/ConfirmationActivity.java
+++ merchant-app/app/src/main/java/com/googleinterns/smb/ConfirmationActivity.java
@@ -9,8 +9,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.Query;
 import com.googleinterns.smb.adapter.EANAdapter;
 
 import java.io.Serializable;
@@ -22,32 +20,19 @@ public class ConfirmationActivity extends AppCompatActivity {
     private static final String TAG = ConfirmationActivity.class.getName();
     public static final String DETECTED_BARCODES = "DETECTED_BARCODES";
 
-    private EANAdapter mAdapter;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation);
         setTitle("Products");
-        initRecyclerView();
-    }
 
-    /**
-     * Fetch data from firebase and initialise recycler view
-     */
-    private void initRecyclerView() {
         // get barcodes from launcher activity
         List<String> barcodes = getBarcodes();
 
-        Query query = FirebaseFirestore.getInstance().collection("products");
-        // filter products containing these barcodes
-        query = query.whereIn("EAN", barcodes);
-
-
         // initialize recycler view
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        mAdapter = new EANAdapter(query);
-        recyclerView.setAdapter(mAdapter);
+        RecyclerView recyclerView = findViewById(R.id.list_item);
+        EANAdapter adapter = new EANAdapter(barcodes);
+        recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this) {
             @Override
             public boolean supportsPredictiveItemAnimations() {
@@ -56,33 +41,12 @@ public class ConfirmationActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        if (mAdapter != null) {
-            mAdapter.startListening();
-        }
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (mAdapter != null) {
-            mAdapter.stopListening();
-        }
-    }
-
     /**
      * utility to get barcodes from serialised data in intent.
      */
     private List<String> getBarcodes() {
-        List<String> barcodes;
-        try {
-            barcodes = (List<String>) getIntent().getSerializableExtra(DETECTED_BARCODES);
-            if (barcodes == null) {
-                barcodes = new ArrayList<>();
-            }
-        } catch (Exception e) {
+        List<String> barcodes = (List<String>) getIntent().getSerializableExtra(DETECTED_BARCODES);
+        if (barcodes == null) {
             barcodes = new ArrayList<>();
         }
         return barcodes;
@@ -90,6 +54,7 @@ public class ConfirmationActivity extends AppCompatActivity {
 
     /**
      * Make intent for confirmation activity.
+     * To display scanned products and confirm to add
      *
      * @param context  initiater activity
      * @param barcodes list of detected barcodes to display
--- merchant-app/app/src/main/java/com/googleinterns/smb/adapter/EANAdapter.java
+++ merchant-app/app/src/main/java/com/googleinterns/smb/adapter/EANAdapter.java
@@ -1,25 +1,25 @@
 package com.googleinterns.smb.adapter;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.Query;
 import com.googleinterns.smb.R;
 
+import java.util.List;
 
-public class EANAdapter extends FirestoreAdapter<EANAdapter.EANViewHolder> {
 
+public class EANAdapter extends RecyclerView.Adapter<EANAdapter.EANViewHolder> {
 
-    public EANAdapter(Query query) {
-        super(query);
+    private List<String> mBarcodes;
+
+    public EANAdapter(List<String> barcodes) {
+        mBarcodes = barcodes;
     }
 
     @NonNull
@@ -31,43 +31,38 @@ public class EANAdapter extends FirestoreAdapter<EANAdapter.EANViewHolder> {
 
     @Override
     public void onBindViewHolder(@NonNull EANViewHolder holder, int position) {
-        holder.bind(getSnapshot(position));
+        holder.bind(mBarcodes.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mBarcodes.size();
+    }
+
+    private void onDelete(int position) {
+        mBarcodes.remove(position);
+        notifyItemRemoved(position);
     }
 
     static class EANViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private TextView mProductName;
-        private TextView mMRP;
-        private TextView mDiscountedPrice;
-        private ImageView mProductImage;
+        private TextView mtextEAN;
+        private EANAdapter mAdapter;
 
-        EANAdapter mAdapter;
-
-        EANViewHolder(@NonNull View itemView, EANAdapter adapter) {
+        private EANViewHolder(@NonNull View itemView, EANAdapter adapter) {
             super(itemView);
-            mProductName = itemView.findViewById(R.id.product_name);
-            mMRP = itemView.findViewById(R.id.mrp);
-            mDiscountedPrice = itemView.findViewById(R.id.discounted_price);
-            mProductImage = itemView.findViewById(R.id.product_image);
+            mtextEAN = itemView.findViewById(R.id.ean_field);
             mAdapter = adapter;
+            itemView.findViewById(R.id.delete_btn).setOnClickListener(this);
         }
 
-        void bind(final DocumentSnapshot documentSnapshot) {
-            String productName = documentSnapshot.getString("product_name");
-            mProductName.setText(productName);
-            String mrp = documentSnapshot.getDouble("MRP").toString();
-            mMRP.setText(mrp);
-            mDiscountedPrice.setText(mrp);
-            String imageURL = documentSnapshot.getString("image_url");
-            Glide.with(mProductImage.getContext())
-                    .load(imageURL)
-                    .fitCenter()
-                    .into(mProductImage);
+        void bind(String barcode) {
+            mtextEAN.setText(barcode);
         }
 
         @Override
         public void onClick(View v) {
-            mAdapter.onDocumentRemoved(getAdapterPosition());
+            mAdapter.onDelete(getAdapterPosition());
         }
 
     }
